b6fcc657aec9f56a86396a55a572177b9c5a4197
sarangr <sarangr@qburst.com>
2021-04-29 17:05:53 +0530

    First one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecomendationsComponent } from './recomendations/recomendations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'recomendation',component:RecomendationsComponent},
+  {path:'favourites',component:RecomendationsComponent},
+  {path:'saved',component:RecomendationsComponent},
+  {path:'home',component:RecomendationsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RecomendationsComponent } from './recomendations/recomendations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecomendationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
